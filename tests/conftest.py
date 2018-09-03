b26cd2053f7f56f672361dc2e3b6bf4c6c93998e
gzxultra <mygladfinger@gmail.com>
2018-09-03 15:52:01 -0400

    Fix test case

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ from random import randint
 
 @pytest.fixture()
 def array1():
-    length = random(10000, 20000)
+    length = randint(10000, 20000)
     yield [randint(0, 99999) for i in range(0, length)]
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -4,4 +4,4 @@ from pyradix import sort
 def test_sort(array1):
     array_for_radix_sort = array1[:]
     array_for_qsort = array1[:]
-    assert sorted(array_for_qsort) = sort(array_for_radix_sort)
+    assert sorted(array_for_qsort) == sort(array_for_radix_sort)
