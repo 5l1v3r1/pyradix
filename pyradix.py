c88f72ba338d5d650a2476f5e9e2fb700de9a53c
gzxultra <mygladfinger@gmail.com>
2018-09-03 15:32:16 -0400

    Implement pyradix.py

---
+++ pyradix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def radix_sort(array, base=10):
+    max_digits = int(math.ceil(math.log(max(array) + 1, base))) # O(N)
+
+    for iter_round in range(0, max_digits):
+        buckets = [[] for i in range(0, base)]
+        for number in array:
+            buckets[number % (base ** (iter_round + 1)) // (base ** iter_round)].append(number)
+
+        array = []
+        for bucket in buckets:
+            array.extend(bucket)
+
+    return array
